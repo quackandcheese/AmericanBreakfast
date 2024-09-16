5f5250cd893ee3c18b43f5b2003fc8552b367274
quackandcheese <quackandcheeseart@gmail.com>
2024-09-16 15:52:42 -0500

    remove triple stack card as lobby option

--- AmericanBreakfast/Mains/Pancakes/TripleStackCard.cs
+++ AmericanBreakfast/Mains/Pancakes/TripleStackCard.cs
@@ -25,7 +25,6 @@ namespace KitchenAmericanBreakfast.Mains
         public override UnlockGroup UnlockGroup => UnlockGroup.Dish;
         public override bool DestroyAfterModUninstall => false;
         public override bool IsUnlockable => true;
-        public override bool IsAvailableAsLobbyOption => true;
         public override int Difficulty => 3;
         public override List<Unlock> HardcodedRequirements => new()
         {
